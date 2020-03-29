987dac08172cda48cb51b319f91922c7559ddc47
Sergey Golub <sergeyglb@outlook.com>
2020-03-29 19:29:01 +0300

    github pages

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -6,6 +6,8 @@ import 'codemirror/lib/codemirror.css';
 import 'codemirror/addon/scroll/simplescrollbars.css';
 import 'codemirror/addon/scroll/simplescrollbars';
 import 'codemirror/addon/display/placeholder';
+import 'codemirror/addon/display/fullscreen.css';
+import 'codemirror/addon/display/fullscreen';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/htmlmixed/htmlmixed';
 import CodeMirror from 'codemirror';
@@ -33,57 +35,128 @@ offButton.addEventListener('click', () => {
 });
 
 setupDemo(
-  'template-simple',
-  'data-simple',
-  'result-simple',
-  'error-simple',
-  `@!(it.html)@
-@(new Date(it.date))@`,
+  'encoded-output-demo',
+  `<p>@(it.user.firstName)@</p>
+<p>@encode(it.user.firstName)@</p>`,
   `{
-  "html": "<h1>Hello World!</h1>",
-  "date": 1585218258461
+  "user": {
+    "firstName": "William"
+  }
+}`,
+);
+setupDemo(
+  'raw-html-output-demo',
+  `<p>@!(it.rawHTML)@</p>`,
+  `{
+  "rawHTML": "<i>Hello!</i>"
+}`,
+);
+setupDemo(
+  'embedded-code-demo',
+  `@{
+  // Any JavaScript code is acceptable in this block
+  const { firstName, secondName } = it.user;
+}@
+<p>@(firstName)@ @(secondName)@</p>`,
+  `{
+  "user": {
+		"firstName": "William",
+  	"secondName": "Smith"
+  }
+}`,
+);
+setupDemo(
+  'if-statement-demo',
+  `@if(it.user && it.user.firstName && it.user.secondName){
+<p>@(it.user.firstName)@</p>
+<p>@(it.user.secondName)@</p>
+} else if (it.user && it.user.firstName) {
+<p>@(it.user.firstName)@</p>
+} else {
+<p>User is not defined</p>
+}@`,
+  `{
+  "user": {
+		"firstName": "William",
+  	"secondName": ""
+  }
+}`,
+);
+setupDemo(
+  'for-while-demo',
+  `<ul>
+  @for(var i = 0, l = it.users.length; i < l; i++){
+  <li>@(it.users[i].firstName)@ @(it.users[i].secondName)@</li>
+  }@
+</ul>
+
+<ul>
+  @{ var i = 0; j = 5; }@
+  @while (i < j) {
+  <li>@(i++)@</li>
+  }@
+</ul>`,
+  `{
+  "users": [
+    {
+      "firstName": "William",
+      "secondName": "Smith"
+    }
+  ]
 }`,
 );
 
-setupDemo('template', 'data', 'result', 'error');
-
-function setupDemo(
-  templateId,
-  dataId,
-  resultId,
-  errorId,
-  templateValue = '',
-  dataValue = '',
-) {
+function setupDemo(demoId, templateValue = '', dataValue = '') {
   const defConfig = {
     mode: 'htmlmixed',
     scrollbarStyle: 'simple',
     tabSize: 2,
     theme: 'vscode',
+    // lineWrapping: true,
+    extraKeys: {
+      F11: function(cm) {
+        cm.setOption('fullScreen', !cm.getOption('fullScreen'));
+        cm.setOption('lineNumbers', cm.getOption('fullScreen'));
+      },
+      Esc: function(cm) {
+        if (cm.getOption('fullScreen')) {
+          cm.setOption('fullScreen', false);
+        }
+        cm.setOption('lineNumbers', cm.getOption('fullScreen'));
+      },
+    },
   };
+  const demo = document.getElementById(demoId);
   const template = CodeMirror.fromTextArea(
-    document.getElementById(templateId) as HTMLTextAreaElement,
+    demo.querySelector('.template>textarea'),
     {
       ...defConfig,
       placeholder: 'Put tamplate code here...',
     },
   );
-  const data = CodeMirror.fromTextArea(
-    document.getElementById(dataId) as HTMLTextAreaElement,
-    {
-      ...defConfig,
-      placeholder: 'Put JSON model here...',
-      mode: 'javascript',
-    },
-  );
-  const result = CodeMirror(document.getElementById(resultId), {
+  const data = CodeMirror.fromTextArea(demo.querySelector('.data>textarea'), {
+    ...defConfig,
+    placeholder: 'Put JSON model here...',
+    mode: 'javascript',
+  });
+  const result = CodeMirror(demo.querySelector('.result-output'), {
     ...defConfig,
     readOnly: true,
   });
-  const error = document.getElementById(errorId);
+  const error = demo.querySelector('.error') as HTMLDivElement;
+
+  const height = 'auto';
+  template.setSize(null, height);
+  data.setSize(null, height);
+  result.setSize(null, height);
+
+  if (false) {
+  } else {
+  }
 
   const onchange = debounce(async () => {
     try {
+      console.log((await parse(template.getValue()));
       const html = await render(
         template.getValue(),
         JSON.parse(data.getValue() || '{}'),
